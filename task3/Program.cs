fb98ad17cc3c32155fdf4092ffd159c830140444
alexei <lexa_nosov99@mail.ru>
2024-03-10 17:10:00 +0500

    Выполнили задание 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        // Произвольный массив
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        // Выводим элементы массива, начиная с конца, в одну строку с использованием рекурсии
+        PrintArrayReverse(array, array.Length - 1);
+    
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            // Выводим текущий элемент с запятой
+            Console.Write(arr[index]);
+
+            // Добавляем запятую, если это не последний элемент
+            if (index > 0)
+            {
+                Console.Write(", ");
+            }
+
+            // Рекурсивно вызываем функцию для предыдущего элемента
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+
