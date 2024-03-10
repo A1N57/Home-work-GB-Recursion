ea8af9c73b60f17fd04430d2e3608ea929874e56
alexei <lexa_nosov99@mail.ru>
2024-03-10 17:05:02 +0500

    Выполнили задание 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿// Запрос у пользователя ввода значений m и n
+        Console.Write("Введите значение m: ");
+        if (int.TryParse(Console.ReadLine(), out int m))
+        {
+            Console.Write("Введите значение n: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                // Вычисляем и выводим значение функции Аккермана
+                int result = AckermannFunction(m, n);
+                Console.WriteLine($"Значение функции Аккермана для m={m} и n={n}: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод для n.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод для m.");
+        }
+    
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
